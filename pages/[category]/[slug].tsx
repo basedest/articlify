272dd82751e61cb82779c636681806acf4d1af3c
@basedest <basedscientist@gmail.com>
2022-05-04 13:16:59 +0300

    upd. 0.5.2: styling fix

--- pages/[category]/[slug].tsx
+++ pages/[category]/[slug].tsx
@@ -9,68 +9,67 @@ const ArticlePage = ({article, page}) => {
   return (
     <>
       <div className="article__container">
-      <div className="article__head">
-        <h1>{article.title}</h1>
-        <div className="article__authordate">
-            <p>{article.author}</p>
-            <p>•</p>
-            <p>{new Date(article.createdAt).toDateString()}</p>
+        <div className="article__head">
+          <h1>{article.title}</h1>
+          <div className="article__authordate">
+              <p>{article.author}</p>
+              <p>•</p>
+              <p>{new Date(article.createdAt).toDateString()}</p>
+          </div>
+          <div className="article__category">{article.category}</div>
+          <div className="article__description">{article.description}</div>
+          <Image className='img' src={img} alt="article image" height="500" width="800" />
+          <TagsList tags={article.tags}/>
         </div>
-        <div className="article__category">{article.category}</div>
-        <div className="article__description">{article.description}</div>
-        <Image className='img' src={img} alt="article image" height="500" width="800" />
-        <TagsList tags={article.tags}/>
-      </div>
-      <div className="article">
-        <article>
-          {
-            page.data.blocks.map(item => {
-              const {id} = item
-              switch (item.type) {
-                case 'paragraph':
-                  return <p key={id} dangerouslySetInnerHTML={{__html:item.data.text}}></p>
-                case 'header':
-                  return item.data.level === 2
-                  ? <h2 key={id}>{item.data.text}</h2>
-                  : <h3 key={id}>{item.data.text}</h3>
-                case 'list':
-                  if (item.data.style === 'ordered') {
-                    return (
-                    <ol key={id}>
-                      {
-                        item.data.items.map((li, i) => <li key={i}>{li}</li>)
-                      }
-                    </ol>
-                    )
-                  }
-                  else {
-                    return (
-                      <ul key={id}>
+        <div className="article">
+          <article>
+            {
+              page.data.blocks.map(item => {
+                const {id} = item
+                switch (item.type) {
+                  case 'paragraph':
+                    return <p key={id} dangerouslySetInnerHTML={{__html:item.data.text}}></p>
+                  case 'header':
+                    return item.data.level === 2
+                    ? <h2 key={id}>{item.data.text}</h2>
+                    : <h3 key={id}>{item.data.text}</h3>
+                  case 'list':
+                    if (item.data.style === 'ordered') {
+                      return (
+                      <ol key={id}>
                         {
                           item.data.items.map((li, i) => <li key={i}>{li}</li>)
                         }
-                      </ul>
+                      </ol>
                       )
-                  }
-                case 'quote':
-                  return <blockquote key={id} cite={item.data.caption}>
-                      {item.data.text}
-                  </blockquote>
-                case 'checklist':
-                  return (
-                    <ul key={id} className='checklist'>
-                      {item.data.items.map((li, i) => <li key={i}>{li.text}</li>)}
-                    </ul>
-                  )
-                case 'delimiter':
-                  return <div key={id} className="article__delimiter"></div>
-              }
-            })
-          }
-        </article>
+                    }
+                    else {
+                      return (
+                        <ul key={id}>
+                          {
+                            item.data.items.map((li, i) => <li key={i}>{li}</li>)
+                          }
+                        </ul>
+                        )
+                    }
+                  case 'quote':
+                    return <blockquote key={id} cite={item.data.caption}>
+                        {item.data.text}
+                    </blockquote>
+                  case 'checklist':
+                    return (
+                      <ul key={id} className='checklist'>
+                        {item.data.items.map((li, i) => <li key={i}>{li.text}</li>)}
+                      </ul>
+                    )
+                  case 'delimiter':
+                    return <div key={id} className="article__delimiter"></div>
+                }
+              })
+            }
+          </article>
+        </div>
       </div>
-      </div>
-      <button onClick={() => console.log(page)}>press me</button>
     </>
   )
 }
