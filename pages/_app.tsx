2a189bea7e982c7b234b906ee076ca126c510aa3
@basedest <basedscientist@gmail.com>
2022-05-12 20:39:05 +0300

    changed _app.tsx (again)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,10 @@
 import '../styles/globals.css'
-import type { AppProps } from 'next/app'
 import { SessionProvider } from "next-auth/react"
 import Footer from '../components/Footer'
 import Header from '../components/Header'
 
 //общая структура каждой страницы
-export default function MyApp({ Component, pageProps }: AppProps) {
+export default function MyApp({ Component, pageProps }) {
   //Если у компонента определена собственная структура
   if (Component.getLayout) {
     return (
