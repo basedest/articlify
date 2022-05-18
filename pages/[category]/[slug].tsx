ed8fbee9388b9c373cb1b59f2f88f0e37e1d0d91
@basedest <basedscientist@gmail.com>
2022-05-18 17:16:21 +0300

    upd.  0.13.1: mobile UI fix

--- pages/[category]/[slug].tsx
+++ pages/[category]/[slug].tsx
@@ -77,9 +77,15 @@ const ArticlePage = ({article}: PageProps) => {
                   return (
                     <ul key={id} className='checklist'>
                       {item.data.items.map((li, i) => <li data-checked={li.checked} key={i}>
-                      {li.checked && <svg xmlns="http://www.w3.org/2000/svg" className='check' viewBox="0 0 20 20" fill="currentColor">
-                        <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd" />
-                      </svg>}
+                      {
+                      li.checked
+                      ?<svg xmlns="http://www.w3.org/2000/svg" className='check' viewBox="0 0 20 20" fill="currentColor">
+                          <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd" />
+                       </svg>
+                      :<svg xmlns="http://www.w3.org/2000/svg" className='check' version="1.1">
+                        <circle cx="8" cy="9" r="5.5" stroke="black" strokeWidth="2" fill="none" clipRule="evenodd" />
+                      </svg> 
+                      }
                         {li.text}
                         </li>)}
                     </ul>
