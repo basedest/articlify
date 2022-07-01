bfa3bba165e618ef2dec5f11f899055062e1293c
@basedest <basedscientist@gmail.com>
2022-07-01 15:44:26 +0300

    upd. 0.15.1: design hotfix

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,32 +14,32 @@ interface IProps {
 const Home: NextPage<IProps> = (props: IProps) => {
  
   return (
-      <>
-        <Head>
-          <title>Articlify</title>
-          <meta name="description" content="Course Project" />
-          <link rel="icon" href="/favicon.ico" />
-        </Head>
-          <div 
-            style={{boxShadow: "inset 0 8px 8px -8px rgb(0 0 0 / 30%), inset 0 -8px 8px -8px rgb(0 0 0 / 30%)"}}
-            className="banner text-white bg-green-500 p-10 mb-8 w-screen"
+    <>
+      <Head>
+        <title>Articlify</title>
+        <meta name="description" content="Course Project" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div
+        style={{boxShadow: "inset 0 8px 8px -8px rgb(0 0 0 / 30%), inset 0 -8px 8px -8px rgb(0 0 0 / 30%)"}}
+        className="banner text-white bg-green-500 p-10 mb-8 w-screen"
+      >
+        <div className="mx-auto px-4 text-center">
+          <h1
+            style={{textShadow: "0px 1px 3px rgb(0 0 0 / 30%)"}}
+            className="title font-['Gotham_Bold'] text-7xl tracking-tight drop-shadow-xl"
           >
-            <div className="mx-auto px-4 text-center">
-              <h1
-                style={{textShadow: "0px 1px 3px rgb(0 0 0 / 30%)"}}
-                className="title font-['Gotham_Bold'] text-7xl tracking-tight drop-shadow-xl"
-              >
-                Articlify
-              </h1>
-              <h2 className="mt-2 font-light text-2xl tracking-wide">A place <em>with</em> articles and <em>without</em> cancel-culture.</h2>
-            </div>
-          </div>
-          <SmartList 
-            articles={props.articles}
-            searchQuery={props.searchQuery}
-            page={props.page}
-          />
-      </>
+            Articlify
+          </h1>
+          <h2 className="mt-2 font-light text-2xl tracking-wide">A place <em>with</em> articles and <em>without</em> cancel-culture.</h2>
+        </div>
+      </div>
+      <SmartList
+        articles={props.articles}
+        searchQuery={props.searchQuery}
+        page={props.page}
+      />
+    </>
   )
 }
 
